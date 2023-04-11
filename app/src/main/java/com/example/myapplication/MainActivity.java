a53fe9fd795043becd2fc8c6cacd7865a41cf68b
Администратор <Alina>
2023-04-11 13:25:50 +0300

    переход на страницу и обратно

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -13,7 +14,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
 
     @Override
@@ -21,44 +23,12 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button4 = findViewById(R.id.button4);
-
-       TextView main_text = findViewById(R.id.maintext);
-        button4.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-            showInfo(main_text.getText().toString(), button4);
-            showInfoAlert("do u want to close app?");
-            }
-        });
-            }
-            public void ButtonClick(View v) { showInfo(((Button) v).getText().toString(),((Button) v) );}
-    private void showInfoAlert(String text) {
-        AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("большая подсказка")
-                .setMessage(text)
-                .setCancelable(false)
-                .setPositiveButton("ofcourse", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                    }
-                })
-                .setNegativeButton("no", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
-    private void showInfo(String text, Button btn) {
-        btn.setText("уже нажали");
-        btn.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show();}
-        };
+
+    public void startnewActivity(View v)
+    {
+        Intent intent = new Intent(this, secondactivity.class);
+        startActivity(intent);
+    }
+}
 
