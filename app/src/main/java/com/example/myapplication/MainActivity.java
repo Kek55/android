b1e4ac284b6b2732d1f111c40389514757d3be7d
Администратор <Alina>
2023-04-11 11:45:24 +0300

    всплывающие окна

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -29,10 +31,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view)
             {
             showInfo(main_text.getText().toString(), button4);
+            showInfoAlert("do u want to close app?");
             }
         });
             }
             public void ButtonClick(View v) { showInfo(((Button) v).getText().toString(),((Button) v) );}
+    private void showInfoAlert(String text) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("большая подсказка")
+                .setMessage(text)
+                .setCancelable(false)
+                .setPositiveButton("ofcourse", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
     private void showInfo(String text, Button btn) {
         btn.setText("уже нажали");
         btn.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
