eec2513e3a4915aaf7128abed4d0b72ffadac2d8
Администратор <Alina>
2023-04-11 10:46:16 +0300

    empty2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,16 +10,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-   private TextView resultTextView;
+    /*  private TextView resultTextView;
     private EditText number_field_1, number_field_2;
-    private Button add_button;
+    private Button add_button; */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultTextView = findViewById(R.id.resultTextView);
+    /*    resultTextView = findViewById(R.id.resultTextView);
         number_field_1 = findViewById(R.id.number_field_1);
         number_field_2 = findViewById(R.id.number_field_2);
         add_button = findViewById(R.id.add_button);
@@ -30,8 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 float num1 = Float.parseFloat(number_field_1.getText().toString());
                 float num2 = Float.parseFloat((number_field_2.getText().toString()));
                 float res = num1 + num2;
-                resultTextView.setText(String.valueOf(res));
+                resultTextView.setText(String.valueOf(res)); */
             }
-        });
-    }
-}
+        };
+
